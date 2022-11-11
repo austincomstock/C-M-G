a14f63b08dedf3932c30dc51585155a312f4bfd0
austincomstock <austinjcomstock@gmail.com>
2022-11-10 17:33:44 -0700

    Removed Image

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import "./About.css";
 
-import afterGravel1 from "../Images/afterGravel1.jpg";
+// import afterGravel1 from "../Images/afterGravel1.jpg";
 
 export const About = () => {
   return (
     <div>
       <h2>About</h2>
-      <p>Gravel Driveways and Gravel Parking Strips</p>
-      <img className="AboutImg" src={afterGravel1} alt="Gravel Parking Strip" />
+      <p>Construction Management Group Specializes in Residential Projects.</p>
+      {/* <img className="AboutImg" src={afterGravel1} alt="Gravel Parking Strip" /> */}
     </div>
   );
 };
