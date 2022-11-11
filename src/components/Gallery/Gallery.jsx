6800f49c4598a9ca56a8430f9dcf9daf4f2e26be
austincomstock <austinjcomstock@gmail.com>
2022-11-11 15:23:19 -0700

    Add more images to Gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -3,7 +3,14 @@ import "./Gallery.css";
 
 // Images
 import afterGravel1 from "../Images/afterGravel1.jpg";
+import afterGravel2 from "../Images/afterGravel2.jpg";
 import fenceCrossland from "../Images/fence-crossland.jpg";
+import fenceGreen from "../Images/fence-green.jpg";
+import miniXFrenchDrain from "../Images/minix-frenchdrain.jpg";
+import miniXTrampoline from "../Images/minix-trampoline.jpg";
+import miniXTreeStump from "../Images/minix-treestump.jpg";
+import rollOffDumpster from "../Images/rolloff-dumpster.jpg";
+import sodRemoval from "../Images/sod-removal.jpg";
 
 export const Gallery = () => {
   return (
@@ -13,13 +20,36 @@ export const Gallery = () => {
         <img
           className="GalleryImg"
           src={afterGravel1}
-          alt="Gravel Parking Strip"
+          alt="Gravel Parking Area"
         />
         <img
           className="GalleryImg"
-          src={fenceCrossland}
+          src={afterGravel2}
           alt="Gravel Parking Strip"
         />
+        <img className="GalleryImg" src={fenceCrossland} alt="Cedar Fence" />
+        <img className="GalleryImg" src={fenceGreen} alt="Fence Mid Build" />
+        <img
+          className="GalleryImg"
+          src={miniXFrenchDrain}
+          alt="Mini Excavator French Drain"
+        />
+        <img
+          className="GalleryImg"
+          src={miniXTrampoline}
+          alt="Mini Excavator In Ground Trampoline"
+        />
+        <img
+          className="GalleryImg"
+          src={miniXTreeStump}
+          alt="Mini Excavator Tree Stump"
+        />
+        <img
+          className="GalleryImg"
+          src={rollOffDumpster}
+          alt="Roll Off Dumpster"
+        />
+        <img className="GalleryImg" src={sodRemoval} alt="Roll Off Dumpster" />
       </div>
     </div>
   );
