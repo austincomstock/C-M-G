e3f231d41b58eaa4e7959f166ce983f676526c6b
austincomstock <austinjcomstock@gmail.com>
2022-11-14 17:18:15 -0700

    Removed comments

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -6,7 +6,6 @@ export const Brand = () => {
   return (
     <div className="Brand1">
       <img src={cmgLogo} className="BrandLogo" alt="logo" />
-      {/* <h1>Construction Management Group</h1> */}
     </div>
   );
 };
