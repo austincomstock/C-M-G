2b25ceaa502b2b340826ba1ec556f2add6662ab0
austincomstock <austinjcomstock@gmail.com>
2022-11-14 17:16:54 -0700

    Added Estimates Component and removed App-Header

--- src/App.js
+++ src/App.js
@@ -5,16 +5,16 @@ import { Brand } from "./components/Brand/Brand";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Gallery } from "./components/Gallery/Gallery";
+import { Estimates } from "./components/Estimates/Estimates";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Brand />
-        <About />
-        <Contact />
-      </header>
+      <Brand />
+      <About />
+      <Contact />
       <Gallery />
+      <Estimates />
     </div>
   );
 }
