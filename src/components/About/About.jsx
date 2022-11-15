7172021fa30f41d76fd8d1762ce64049aa4a34a7
austincomstock <austinjcomstock@gmail.com>
2022-11-14 17:15:50 -0700

    Removed old comments

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import "./About.css";
 
-// import afterGravel1 from "../Images/afterGravel1.jpg";
-
 export const About = () => {
   return (
     <div>
       <h2>About</h2>
       <p>Construction Management Group Specializes in Residential Projects.</p>
-      {/* <img className="AboutImg" src={afterGravel1} alt="Gravel Parking Strip" /> */}
     </div>
   );
 };
