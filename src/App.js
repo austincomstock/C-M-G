d5bd24e536e7f88e605f4ed930f148b8499501a6
austincomstock <austinjcomstock@gmail.com>
2022-11-21 15:25:21 -0700

    Installed & added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import cmgLogo from "./components/Images/cmgLogo.jpg";
 
 // COMPONENTS
-import { Brand } from "./components/Brand/Brand";
+// import { Home } from "./components/Home/Home";
 import { About } from "./components/About/About";
 import { Contact } from "./components/Contact/Contact";
 import { Gallery } from "./components/Gallery/Gallery";
@@ -10,14 +12,56 @@ import { ServiceArea } from "./components/ServiceArea/ServiceArea";
 
 function App() {
   return (
-    <div className="App">
-      <Brand />
-      <About />
-      <Contact />
-      <Gallery />
-      <Estimates />
-      <ServiceArea />
-    </div>
+    <Router>
+      <div className="App">
+        {/* <Home /> */}
+
+        <section>
+          <h1>Construction Management Group</h1>
+          <img src={cmgLogo} className="BrandLogo" alt="logo" />
+        </section>
+
+        {/* Navigation */}
+        <nav>
+          <ul className="NavigationUL">
+            {/* <li>
+              <Link to="/">Home</Link>
+            </li> */}
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link to="/gallery">Gallery</Link>
+            </li>
+            <li>
+              <Link to="/estimates">Estimates</Link>
+            </li>
+            <li>
+              <Link to="/servicearea">Service Area</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* <Home /> */}
+        {/* <About /> */}
+        {/* <Contact /> */}
+        {/* <Gallery /> */}
+        {/* <Estimates /> */}
+        {/* <ServiceArea /> */}
+
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/estimates" element={<Estimates />} />
+          <Route path="/servicearea" element={<ServiceArea />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
