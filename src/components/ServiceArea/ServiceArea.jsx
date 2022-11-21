63fe00147260d0a74d7a0044b2892e18044ff810
austincomstock <austinjcomstock@gmail.com>
2022-11-21 16:11:22 -0700

    Added Image and Text to ServiceArea component

--- src/components/ServiceArea/ServiceArea.jsx
+++ src/components/ServiceArea/ServiceArea.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./ServiceArea.css";
+import cmgServiceArea from "../Images/cmg-service-area.jpg";
 
 export const ServiceArea = () => {
   return (
     <div className="ServiceArea1">
       <h2>ServiceArea</h2>
+      <p>
+        The Construction Management Group proudly services the Salt Lake County
+        area.
+      </p>
+      <img
+        src={cmgServiceArea}
+        className="cmgServiceAreaImage"
+        alt="service area map"
+      />
     </div>
   );
 };
