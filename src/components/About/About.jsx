70b73ec874c432ae23b955dc4c4d37e82d6e3f28
austincomstock <austinjcomstock@gmail.com>
2022-11-22 15:39:21 -0700

    Added className & <ul> with items

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,9 +3,15 @@ import "./About.css";
 
 export const About = () => {
   return (
-    <div>
+    <div className="About1">
       <h2>About</h2>
       <p>Construction Management Group Specializes in Residential Projects.</p>
+      <ul>
+        <li>Gravel Driveways & Parking Strips</li>
+        <li>Property Cleanup & Maintenance</li>
+        <li>Sod Removal</li>
+        <li>Landscaping Material Delivery</li>
+      </ul>
     </div>
   );
 };
