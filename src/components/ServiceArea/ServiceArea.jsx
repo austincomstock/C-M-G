791811659314436ae76e0950231045367cf25a98
austincomstock <austinjcomstock@gmail.com>
2022-11-23 13:27:56 -0700

    Added space to Service Area

--- src/components/ServiceArea/ServiceArea.jsx
+++ src/components/ServiceArea/ServiceArea.jsx
@@ -5,7 +5,7 @@ import cmgServiceArea from "../Images/cmg-service-area.jpg";
 export const ServiceArea = () => {
   return (
     <div className="ServiceArea1">
-      <h2>ServiceArea</h2>
+      <h2>Service Area</h2>
       <p>
         The Construction Management Group proudly services the Salt Lake County
         area.
