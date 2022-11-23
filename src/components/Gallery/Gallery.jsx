285d6533168ce5ff52624c5e6bc9e98a3c0757a0
austincomstock <austinjcomstock@gmail.com>
2022-11-23 13:03:59 -0700

    Removed unnecessary jsx fragment

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -14,63 +14,53 @@ import sodRemoval from "../Images/sod-removal.jpg";
 
 export const Gallery = () => {
   return (
-    <>
-      <div className="Gallery1">
-        <h2>Gallery</h2>
-        <div className="row">
-          <div className="column">
-            <img
-              className="galleryImg"
-              src={afterGravel1}
-              alt="Gravel Parking Area"
-            />
-            <img
-              className="galleryImg"
-              src={afterGravel2}
-              alt="Gravel Parking Strip"
-            />
-            <img
-              className="galleryImg"
-              src={fenceCrossland}
-              alt="Cedar Fence"
-            />
-          </div>
-          <div className="column">
-            <img
-              className="galleryImg"
-              src={fenceGreen}
-              alt="Fence Mid Build"
-            />
-            <img
-              className="galleryImg"
-              src={miniXFrenchDrain}
-              alt="Mini Excavator French Drain"
-            />
-            <img
-              className="galleryImg"
-              src={miniXTrampoline}
-              alt="Mini Excavator In Ground Trampoline"
-            />
-          </div>
-          <div className="column">
-            <img
-              className="galleryImg"
-              src={miniXTreeStump}
-              alt="Mini Excavator Tree Stump"
-            />
-            <img
-              className="galleryImg"
-              src={rollOffDumpster}
-              alt="Roll Off Dumpster"
-            />
-            <img
-              className="galleryImg"
-              src={sodRemoval}
-              alt="Roll Off Dumpster"
-            />
-          </div>
+    <div className="Gallery1">
+      <h2>Gallery</h2>
+      <div className="row">
+        <div className="column">
+          <img
+            className="galleryImg"
+            src={afterGravel1}
+            alt="Gravel Parking Area"
+          />
+          <img
+            className="galleryImg"
+            src={afterGravel2}
+            alt="Gravel Parking Strip"
+          />
+          <img className="galleryImg" src={fenceCrossland} alt="Cedar Fence" />
+        </div>
+        <div className="column">
+          <img className="galleryImg" src={fenceGreen} alt="Fence Mid Build" />
+          <img
+            className="galleryImg"
+            src={miniXFrenchDrain}
+            alt="Mini Excavator French Drain"
+          />
+          <img
+            className="galleryImg"
+            src={miniXTrampoline}
+            alt="Mini Excavator In Ground Trampoline"
+          />
+        </div>
+        <div className="column">
+          <img
+            className="galleryImg"
+            src={miniXTreeStump}
+            alt="Mini Excavator Tree Stump"
+          />
+          <img
+            className="galleryImg"
+            src={rollOffDumpster}
+            alt="Roll Off Dumpster"
+          />
+          <img
+            className="galleryImg"
+            src={sodRemoval}
+            alt="Roll Off Dumpster"
+          />
         </div>
       </div>
-    </>
+    </div>
   );
 };
