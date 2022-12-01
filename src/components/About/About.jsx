eec3b4d26bf07bdea525bcaf99f873f3d519d71a
austincomstock <austinjcomstock@gmail.com>
2022-12-01 12:35:14 -0700

    Added className to <li>, updated sod removal text

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,12 +5,12 @@ export const About = () => {
   return (
     <div className="About1">
       <h2>About</h2>
-      <p>Construction Management Group Specializes in Residential Projects.</p>
-      <ul>
-        <li>Gravel Driveways & Parking Strips</li>
-        <li>Property Cleanup & Maintenance</li>
-        <li>Sod Removal</li>
-        <li>Landscaping Material Delivery</li>
+      <p>Construction Management Group Specializes In Residential Projects</p>
+      <ul className="AboutUL">
+        <li className="AboutLI">Gravel Driveways & Parking Strips</li>
+        <li className="AboutLI">Sod Removal & Installation</li>
+        <li className="AboutLI">Property Cleanup & Maintenance</li>
+        <li className="AboutLI">Landscaping Material Delivery</li>
       </ul>
     </div>
   );
