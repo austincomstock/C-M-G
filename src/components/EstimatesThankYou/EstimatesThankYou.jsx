c24253a3ddfa67b843f52a3a79bdaf0e2f2e8b3e
austincomstock <austinjcomstock@gmail.com>
2022-12-02 12:11:22 -0700

    Added className and css style

--- src/components/EstimatesThankYou/EstimatesThankYou.jsx
+++ src/components/EstimatesThankYou/EstimatesThankYou.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import "./EstimatesThankYou.css";
 
 export const EstimatesThankYou = () => {
   return (
-    <div>
+    <div className="EstimatesThankYou1">
       <h3>Thank You! Your form has been submitted.</h3>
     </div>
   );
